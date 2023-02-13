3b2c6d9510e9baa8e10a1acda57b093ae9007fc5
AtaAllA <100690634+AtaAllA@users.noreply.github.com>
2023-02-13 15:26:42 +0200

    node js lab 1

---
+++ index.js
@@ -0,0 +1,88 @@
+const fs=require('fs');
+const { parse } = require('path');
+
+// let studentData= {id:data.length+1  , name:process.argv[3] , grade:process.argv[4]};
+
+
+
+if(process.argv[2]=="add")
+{
+    let data=JSON.parse( fs.readFileSync("student12.txt",'utf-8'))
+    console.log(data)
+
+    let studentData= {id:data.length+1  , name:process.argv[3] , grade:process.argv[4]};
+
+    // console.log(studentData);
+
+    data.push(studentData);
+
+    fs.writeFileSync("student12.txt",JSON.stringify(data));
+
+    console.log(data)
+    
+}
+
+
+else if (process.argv[2] == "list"){
+
+fs.readFile('student12.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
+
+
+}
+
+
+
+else if(process.argv[2]=="edit")
+{
+    let data=JSON.parse( fs.readFileSync("student12.txt",'utf-8'))
+    data[parseInt(process.argv[4])].grade = parseInt(process.argv[3]);
+
+    // data[parseInt( process.argv[3])]=process.argv[4];
+    fs.writeFileSync("student12.txt",JSON.stringify(data));
+}
+
+
+else if(process.argv[2]=="del")
+{
+    let data=JSON.parse( fs.readFileSync("student12.txt",'utf-8'))
+
+   data.splice(parseInt(process.argv[3]),1);
+   
+    fs.writeFileSync("student12.txt",JSON.stringify(data));
+}
+
+
+
+
+
+else if (process.argv[2] == "sum"){
+    sum()
+}
+
+
+
+else
+{
+    console.log(err);
+}
+
+
+function sum(){
+    let sum = 0
+    for (let i = 0 ; i < studentData ; i++ )
+    sum +=parseInt(studentData[i].grade)
+}
+
+
+
+
+
+
+
+
